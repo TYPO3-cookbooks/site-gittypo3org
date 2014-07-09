15765ade532028075e1b642f40009c424a294263
Steffen Gebert <steffen.gebert@typo3.org>
2014-07-09 23:22:22 +0200

    include_recipe gitweb and git-daemon

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,5 +2,8 @@
 # move all the git-server stuff from roles/site-reviewtypo3org and roles/gerrit to this cookbook
 
 
-include_recipe "chef-vault"
+include_recipe "t3-chef-vault"
+include_recipe "gitweb"
+include_recipe "git-daemon"
+
 include_recipe "site-gittypo3org::worker"
