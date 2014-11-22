bf681727f809f6e0ddcf5246cda2840e5348a848
Steffen Gebert <steffen.gebert@typo3.org>
2014-11-22 23:10:45 +0100

    Bump version to 0.1.8

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Steffen Gebert / TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/configures git.typo3.org"
-version          "0.1.7"
+version          "0.1.8"
 
 depends "ssl_certificates", "= 1.1.3"
 depends "t3-chef-vault", "= 1.0.1"
