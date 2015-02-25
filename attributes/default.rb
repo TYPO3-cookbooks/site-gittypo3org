c1b354f3d93238f3703aca0146aa4cf99d928195
Peter Niederlag <peter.niederlag@typo3.org>
2015-02-25 11:48:17 +0100

    set git-daemon timeout to 7200

--- attributes/default.rb
+++ attributes/default.rb
@@ -23,6 +23,7 @@ end
 default['git-daemon']['home'] = "/var/git"
 default['git-daemon']['path'] = "/var/git/repositories"
 default['git-daemon']['max_connections'] = "60"
+default['git-daemon']['timeout'] = "7200"
 
 default['gitweb']['path'] = "/var/git/repositories"
 default['gitweb']['ssl'] = true
