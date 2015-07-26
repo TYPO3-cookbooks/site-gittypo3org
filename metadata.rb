36e7575aff51b51ab3a6bdbc720bff8769ff7384
Steffen Gebert <steffen.gebert@typo3.org>
2015-07-26 11:24:48 +0200

    Loosen dependency to zabbix-custom-checks

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          "Apache 2.0"
 description      "Installs/configures git.typo3.org"
 version          "0.1.15"
 
-depends "zabbix-custom-checks", "= 0.2.0"
+depends "zabbix-custom-checks", "~> 0.2.1"
 depends "ssl_certificates", "= 1.1.3"
 depends "t3-chef-vault", "= 1.0.1"
 depends "t3-gitweb", "= 0.1.7"
