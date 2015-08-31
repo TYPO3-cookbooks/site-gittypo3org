9fc41bfe925c5b58d0141a1609cdb0720ea1efbd
Steffen Gebert <steffen.gebert@typo3.org>
2015-08-31 21:49:11 +0200

    Bump version to 0.1.17

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Steffen Gebert / TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/configures git.typo3.org"
-version          "0.1.16"
+version          "0.1.17"
 
 depends "zabbix-custom-checks", "~> 0.2.1"
 depends "ssl_certificates", "= 1.1.3"
