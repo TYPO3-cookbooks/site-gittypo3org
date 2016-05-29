8e59b495298818a60b3755744a2ca358faf17f35
Steffen Gebert <steffen.gebert@typo3.org>
2016-05-29 18:38:57 +0200

    Depend on t3-base

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,13 @@ license          "Apache 2.0"
 description      "Installs/configures git.typo3.org"
 version          "0.1.17"
 
+depends "t3-base"
+
 depends "zabbix-custom-checks", "~> 0.2.1"
-depends "ssl_certificates", "= 1.1.3"
-depends "t3-chef-vault", "= 1.0.1"
-depends "t3-gitweb", "= 0.1.8"
-depends "git-daemon", "= 1.2.0"
-depends "apt"
-depends "runit"
+depends "ssl_certificates", "~> 1.1.3"
+depends "t3-chef-vault", "~> 1.0.1"
+depends "t3-gitweb", "~> 0.1.8"
+depends "git-daemon", "~> 1.2.0"
+
+depends "runit", "= 1.7.2"
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 # todo
 # move all the git-server stuff from roles/site-reviewtypo3org and roles/gerrit to this cookbook
 
+include_recipe "t3-base"
 
 include_recipe "t3-chef-vault"
 include_recipe "t3-gitweb"
