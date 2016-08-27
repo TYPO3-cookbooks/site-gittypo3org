898dab43ab4146a2d2ce56956014c7e8fb472b58
Steffen Gebert <st+github@st-g.de>
2016-08-27 15:00:06 +0200

    [TASK] Bump version to 0.1.19

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Steffen Gebert / TYPO3 Association"
 maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/configures git.typo3.org"
-version          "0.1.18"
+version          "0.1.19"
 
 depends "t3-base"
 
