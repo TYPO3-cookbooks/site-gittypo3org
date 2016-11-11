2436aa3bf7250cd7d5aca9d9c35b9305bfc937de
Steffen Gebert <steffen.gebert@typo3.org>
2016-11-11 23:42:31 +0100

    [BREAKING] Drop message queue

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,25 +1,3 @@
-# generic defaults
-
-default['site-gittypo3org']['amqp']['server'] = nil
-default['site-gittypo3org']['amqp']['user'] = nil
-default['site-gittypo3org']['amqp']['vhost'] = nil
-
-# override defaults for production
-if node.chef_environment == 'production'
-  default['site-gittypo3org']['amqp'] = {
-    'server' => 'mq.typo3.org',
-    'user' => 'gittypo3org',
-    'vhost' => 'infrastructure'
-  }
-# override defaults for pre-production
-elsif node.chef_environment == 'pre-production'
-  default['site-gittypo3org']['amqp'] = {
-    'server' => 'mq.typo3.org',
-    'user' => 'devgittypo3org',
-    'vhost' => 'infrastructure_dev'
-  }
-end
-
 default['git-daemon']['home'] = "/var/git"
 default['git-daemon']['path'] = "/var/git/repositories"
 default['git-daemon']['max_connections'] = "60"
