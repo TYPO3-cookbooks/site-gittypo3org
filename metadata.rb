5c848279a4a26ab67f26c9191b431f18ace26ddc
Steffen Gebert <steffen.gebert@typo3.org>
2016-11-13 13:18:30 +0100

    [TASK] Bump dependency gitweb~>0.2.0

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ depends "t3-base"
 depends "zabbix-custom-checks", "~> 0.2.1"
 depends "ssl_certificates", "~> 1.1.3"
 depends "t3-chef-vault", "~> 1.0.1"
-depends "t3-gitweb", "~> 0.1.8"
+depends "t3-gitweb", "~> 0.2.0"
 depends "git-daemon", "~> 1.2.0"
 
 depends "runit", "= 1.7.6"
